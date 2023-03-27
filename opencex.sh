7dce8c68963abbd31b4de188357c589ab4bf4b9d
msergeev <mail@msergeev.ru>
2023-03-27 18:30:12 +0300

    add docker install if it is missing

--- opencex.sh
+++ opencex.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ $(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+    echo "Docker not found, install docker..."
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io git docker-compose-plugin
+    echo "Docker has been successfully installed."
+else
+    echo "Docker already installed."
+fi
+
 
 mkdir /app/opencex -p
 cd /app/opencex || exit
