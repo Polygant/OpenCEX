fec9ca5bcb849d664abef6c9363c6e9bfe4697b6
msergeev <mail@msergeev.ru>
2023-05-11 16:08:50 +0300

    master

--- opencex.sh
+++ opencex.sh
@@ -18,10 +18,10 @@ fi
 
 mkdir /app/opencex -p
 cd /app/opencex || exit
-git clone -b stage https://github.com/Polygant/OpenCEX-backend.git ./backend
-git clone -b stage https://github.com/Polygant/OpenCEX-frontend.git ./frontend
-git clone -b stage https://github.com/Polygant/OpenCEX-static.git ./nuxt
-git clone -b stage https://github.com/Polygant/OpenCEX-JS-admin.git ./admin
+git clone https://github.com/Polygant/OpenCEX-backend.git ./backend
+git clone https://github.com/Polygant/OpenCEX-frontend.git ./frontend
+git clone https://github.com/Polygant/OpenCEX-static.git ./nuxt
+git clone https://github.com/Polygant/OpenCEX-JS-admin.git ./admin
 
 echo "`cat <<YOLLOPUKKI
 
